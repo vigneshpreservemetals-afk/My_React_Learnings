c395ffdff71b8854c830f3275197abdc3188b41a
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-05-18 13:51:08 +0530

    added

---
+++ src/Website_Pages/DemoPage1.jsx
@@ -0,0 +1,10 @@
+
+export default function DemoPage1() {
+
+    return <div>
+        <h1> Welcome to React JS World </h1>
+        <p> Developed by: Vignesh Baskaran </p>
+        <p> This is a simple demo page for React JS   </p>
+        </div>
+
+}
