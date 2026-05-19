cffaa5deb2b28c75975d75c0e8cfcb4ff267f1e6
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-05-19 22:20:43 +0530

    updated episode 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import DemoPage1 from './Website_Pages/DemoPage1';
-import DemoPage2 from './Website_Pages/DemoPage2';
-import DemoPage3 from './Website_Pages/DemoPage3';
-import DemoPage4 from './Website_Pages/DemoPage4';
-import DemoPage5 from './Website_Pages/DemoPage5';
-import DemoPage6 from './Website_Pages/DemoPage6';
-import DemoPage7 from './Website_Pages/DemoPage7';
+import Homepage from './pages/a-home/HomePage';
+import ContactPage from './pages/c-contact/ContactPage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
-  <DemoPage7 />
+  <Homepage/>
+  <ContactPage/>
+
   </React.StrictMode>
 );
