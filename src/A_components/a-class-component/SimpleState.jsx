ebdf5635e292d4a584386fd5890208c9d461a64a
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-05-19 20:55:41 +0530

    added function/ class component

---
+++ src/A_components/a-class-component/SimpleState.jsx
@@ -0,0 +1,47 @@
+
+import React from "react";
+
+export default class SimpleState extends React.component{
+
+    constructor(props){
+        super(proper)
+
+        this.state={
+            instituteName:"CredoSystems",
+            location:"Velachery",
+        
+            student:{
+            name:"Vignesh Baskaran",
+            batchNo:260274,
+            qualification:"BE(Mechanical)",
+            course:"React Full-Stack Developer"
+        },
+
+        mentor:{
+            name:"Saravanan",
+            role:"Full-Stack Trainer",
+            experience: "5 years"
+
+        }
+
+    }
+
+
+    render() {
+        return 
+        <div>
+            <h3> {this.state.instituteName + ',' + this.state.location} </h3>
+
+            <div className="StudentDetails">
+                <p> {this.state.student.name + ',' + this.state.student.batchNo} </p>
+                <p> {this.state.student.qualification} </p>
+                <p> {this.state.student.course} </p>
+            </div>
+
+            <div className="MentorDetails">
+                <p> {this.state.mentor.name + ',' +this.state.mentor.role + ',' + this.state.men.experience} </p>
+            </div>
+        </div>
+    }
+}
+}
---
+++ src/A_components/b-function_component/HelloFunction.js
@@ -0,0 +1,10 @@
+
+
+export default function HelloFunction(){
+
+    return <div>
+
+        <h3> Hello am a Function Component </h3>
+        
+    </div>
+}
