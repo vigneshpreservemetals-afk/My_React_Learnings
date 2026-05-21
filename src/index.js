5ac6528047568ff9c59466fa31403f59b234c95f
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-05-21 07:44:05 +0530

    updated episode4

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import A_Parent from './pages/c-props_callback/A_Parent';
+
+import Demo1 from './pages/demo-page1/Demo1';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-  <A_Parent/>
+  <Demo1/>
   
 
   </React.StrictMode>
