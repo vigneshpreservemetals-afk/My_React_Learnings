800aa11464406cfd82557a41d67ef2a449196c1d
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-05-24 22:01:24 +0530

    updated  demopage 7

--- src/pages/demo-page7/DemoPage7.jsx
+++ src/pages/demo-page7/DemoPage7.jsx
@@ -12,11 +12,16 @@ export default function DemoPage7() {
         <div> <h4> Employee Information </h4>
 
             <div> <span> <strong> Employee Name: </strong> </span>
-                <span> {EmployeeInfo.Name}  </span> </div>
+                <span> {EmployeeInfo.Name}  </span> 
+
+            <button onClick={() => SetEmployeeInfo({...EmployeeInfo,Name:"Krishna"})}> Update Name </button>    
+            </div>
+
 
             <div> <span> <strong> Employee Address: </strong> </span>
                 <span> {EmployeeInfo.Address.StreetName}  </span> 
 
+           
             <button onClick={() => SetEmployeeInfo({...EmployeeInfo, 
                 Address:{...EmployeeInfo.Address, StreetName:"1st Street"} })}> Change Street Name </button>
             </div>
