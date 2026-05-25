f3fd1d1849d42167fe5ae61c15e781aa034990e8
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-05-25 19:21:12 +0530

    added search box event

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-import StudentListPreparation from './pages/StudentNameListPreparation'
+import SearchBoxEvent from './pages/SearchBoxEvent';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-  <StudentListPreparation/>
+  <SearchBoxEvent/>
   
 
   </React.StrictMode>
