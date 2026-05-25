7a402253f1d8072d17b7cf73347951e1c0740d01
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-05-25 07:24:46 +0530

    added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-import DemoPage8 from './pages/demo-page8/DemoPage8';
+import DemoPage9 from './pages/demo-page9/DemoPage9';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-  <DemoPage8/>
+  <DemoPage9/>
   
 
   </React.StrictMode>
