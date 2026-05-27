4fdafd1ab9bbb8c961f7780a2bc49aabee38b50d
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-05-27 08:14:53 +0530

    basicforms added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-import SelectBoxEvent1 from './pages/SelectBoxEvent1'
+import BasicForms from '../src/pages/BasicForms/BasicForms'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-  <SelectBoxEvent1/>
+  <BasicForms/>
   
 
   </React.StrictMode>
