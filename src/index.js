185bf61a88dc319e725002b52929c56b9f8435cb
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-01 07:02:01 +0530

    JSON form added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-import FormikYup from '../src/pages/D-formik-yup-validation/FormikYup'
+import JsonLoginForm from './pages/B_JSON-form/JsonLoginForm';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-  <FormikYup/>
+  <JsonLoginForm/>
   
 
   </React.StrictMode>
