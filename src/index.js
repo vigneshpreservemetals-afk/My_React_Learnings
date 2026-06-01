3cbcf57d527ce718ad93b8d2e8d217a25206d9ca
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-01 21:33:38 +0530

    added useEffect basic understand file

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-import JsonLoginForm from './pages/B_JSON-form/JsonLoginForm';
+import LifeCycleMethod from '../src/pages/LifeCycleMethods/LifeCycleMethods'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-  <JsonLoginForm/>
+  <LifeCycleMethod/>
   
 
   </React.StrictMode>
