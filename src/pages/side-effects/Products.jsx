c0eba5a1ecabccfeda5ef2d67dfd8f185ab40cf2
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-02 18:48:44 +0530

    added

---
+++ src/pages/side-effects/Products.jsx
@@ -0,0 +1,15 @@
+
+import { useEffect,useState } from "react";
+
+export default function Products() {
+
+    const[productList,setProductList] = useState()
+    const[page,setPage] = useState(1)
+
+useEffect(() => {
+
+    const controller = new AbortController( )
+})
+
+
+}
