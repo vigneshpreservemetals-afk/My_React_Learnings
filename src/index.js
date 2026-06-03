a2d829fcb03d794684a22a83da1d9c4a2c8b3f48
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-03 08:08:11 +0530

    added ep11 styles

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-import MountingPhase from '../src/pages/side-effects/a_mounting-phase/MountingPhase'
+import AboutPage from './pages/about/AboutPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-  <MountingPhase/>
+  <AboutPage/>
   
 
   </React.StrictMode>
