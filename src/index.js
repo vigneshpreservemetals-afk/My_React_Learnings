8768b32f9c6bc020b3fa536f2aedeef431688974
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-04 08:00:02 +0530

    added contat page

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import AboutPage from './pages/about/AboutPage';
+import Contactpage from './pages/contact/Contactpage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-  <AboutPage/>
+  <Contactpage/>
   
 
   </React.StrictMode>
