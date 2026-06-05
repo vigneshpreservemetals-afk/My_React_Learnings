6c2d4f099d21cea3b944a209f4b1e999a46b024e
vignesh.preservemetals@gmail.com <vignesh@gmail.comreset>
2026-06-05 10:52:16 +0530

    Added params

--- src/Core/json/reusuable-components/Header.jsx
+++ src/Core/json/reusuable-components/Header.jsx
@@ -8,5 +8,6 @@ return <div className="bg-primary">
 
     <p> <Link to ={"/about"} className="text-white" > About </Link> </p>
     <p> <Link to ={"/contact"} className="text-white" > Contact </Link> </p>
+    <p> <Link to ={"/home"} className="text-white" > Home </Link> </p>
 </div>
 }
--- src/pages/router/RoutingOutlet.jsx
+++ src/pages/router/RoutingOutlet.jsx
@@ -3,6 +3,7 @@ import{BrowserRouter,Routes,Route} from 'react-router'
 
 import AboutPage from '../about/AboutPage'
 import Contactpage from '../contact/Contactpage'
+import Homepage from '../home/Homepage'
 import PageNotFound from '../pageNotFound/PageNotFound'
 import Header from '../../core/json/reusuable-components/Header'
 
@@ -17,6 +18,7 @@ export default function RoutingOutlet() {
             <Route index element ={<AboutPage/>} />  
             <Route path= "about" element ={<AboutPage/>} />  
             <Route path= "contact" element ={<Contactpage/>} />  
+            <Route path= "home" element={<Homepage/>} />   
             <Route path="*" element={<PageNotFound/>} />
         </Routes>        
         </BrowserRouter>
